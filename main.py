9c3a88febed9807e30ef8a8d47ba3006e62a0a79
Bashar Al-Saify <bashar.alsaify@yahoo.com>
2025-07-05 01:40:33 +0200

    saving data in dedicated file

--- main.py
+++ main.py
@@ -1,20 +1,27 @@
 # Source: Polygon.io
 
 from dotenv import load_dotenv
-import os
 import requests
+import json
+import os
+
 
 # Load the environment variables from .env file
 load_dotenv()
-
 API_KEY= os.environ.get('POLYGON_IO_API_KEY')
 
+
+output_file = "APPLE_STOCK.json"
+
 url = (
-    'https://api.polygon.io/v2/aggs/ticker/AAPL/range/1/day/2024-06-01/2025-06-30'
+    'https://api.polygon.io/v2/aggs/ticker/AAPL/range/1/day/2024-06-01/2025-07-05'
     f'?adjusted=true&sort=asc&apiKey=' + API_KEY
 )
 
 response = requests.get(url)
 data = response.json()
+
+with open(output_file, "w") as file:
+    json.dump(data, file, indent=4)
 
 print(data)
