3a39c5013e965ca18b7ac806decd10724c9bd1c5
Bashar Al-Saify <bashar.alsaify@yahoo.com>
2025-07-05 02:16:06 +0200

    Scalable get request in all stocks in file stockName.txt

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 
 from dotenv import load_dotenv
 import requests
+import time
 import json
 import os
 
@@ -11,17 +12,20 @@ load_dotenv()
 API_KEY= os.environ.get('POLYGON_IO_API_KEY')
 
 
-output_file = "APPLE_STOCK.json"
 
-url = (
-    'https://api.polygon.io/v2/aggs/ticker/AAPL/range/1/day/2024-06-01/2025-07-05'
-    f'?adjusted=true&sort=asc&apiKey=' + API_KEY
-)
 
-response = requests.get(url)
-data = response.json()
-
-with open(output_file, "w") as file:
-    json.dump(data, file, indent=4)
-
-print(data)
+with open("stockName.txt") as f:
+    for line in f:
+        stock = line.strip()
+        print(f"Fetching: {stock}")
+        url = (
+            f'https://api.polygon.io/v2/aggs/ticker/{stock}/range/1/day/2024-06-01/2025-07-05'
+            f'?adjusted=true&sort=asc&apiKey={API_KEY}'
+        )
+        response = requests.get(url)
+        data = response.json()
+        print(data)
+        output_file = f"output_files/{stock}_STOCK.json"
+        with open(output_file, "w") as file:
+            json.dump(data, file, indent=4)
+        time.sleep(15)  # delay to avoid rate limits
